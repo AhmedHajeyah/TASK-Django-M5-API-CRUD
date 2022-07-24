346a8bb75dee887aa3c53c1bdac589d4fe6d3aa1
Ahmed Hajeyah <hajeyah@gmail.com>
2022-07-24 19:05:09 +0300

    Solve the Solution od API CRUD

--- airport/urls.py
+++ airport/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("flights/", flight_views.FlightListView.as_view(), name="flights-list"),
     path("bookings/", flight_views.BookingListView.as_view(), name="bookings-list"),
+    path("booking-details/<int:booking_id>/", flight_views.BookingDetailView.as_view()),
+    path("booking-update/<int:booking_id>/", flight_views.BookingUpdateView.as_view()),
+    path("cancel-booking/<int:booking_id>/", flight_views.BookingDeleteView.as_view()),
+
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,9 @@
+from cgitb import lookup
 from datetime import datetime
 
-from rest_framework.generics import ListAPIView
-
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from .models import Booking, Flight
-from .serializers import BookingListSerializer, FlightListSerializer
+from .serializers import BookingListSerializer, FlightListSerializer,BookingListSerializer, BookingDetailSerializer , BookingUpdateSerializer, BookingDeleteSerializer
 
 
 class FlightListView(ListAPIView):
@@ -14,3 +14,29 @@ class FlightListView(ListAPIView):
 class BookingListView(ListAPIView):
     queryset = Booking.objects.filter(date__gte=datetime.today())
     serializer_class = BookingListSerializer
+
+"""Create an API detail view:
+Which will display the following fields for the Booking object:
+id
+flight
+date
+passengers"""
+
+class BookingDetailView(RetrieveAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingDetailSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "booking_id"
+
+
+class BookingUpdateView(UpdateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingUpdateSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "booking_id"
+
+class BookingDeleteView(DestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingDeleteSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "booking_id"
